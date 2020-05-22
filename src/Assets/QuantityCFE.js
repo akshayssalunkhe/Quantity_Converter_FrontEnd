3459395251edb73c90b288b8496d30c15d1f9e4e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-05-22 23:12:55 +0530

    REFACTORED:Code For Backend Connectivity

--- src/Assets/QuantityCFE.js
+++ src/Assets/QuantityCFE.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import './Quantity.css'
+import axios from 'axios'
 
 class QuantityConversion extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            unitType: ['Length'],
-            units: ['Inch', 'Feet', 'Yard', 'Centimeter'],
+            unitType: ['LENGTH'],
+            units: ['INCH', 'FEET', 'YARD', 'CENTIMETER'],
             selectedUnitType: '',
+            inputValue:0,
+            inputUnit:'INCH',
+            outputUnit:'INCH',
+            outputValue:0,
         };
         this.changeUnitType = this.changeUnitType.bind(this);
     }
@@ -16,10 +21,10 @@ class QuantityConversion extends Component {
     componentDidMount() {
         this.setState({
             unitType: [
-                {name: 'Length', units: ['Inch', 'Feet', 'Yard', 'Centimeter']},
-                {name: 'Weight', units: ['Kilogram', 'Gram', 'Tonne']},
-                {name: 'Volume', units: ['Litre', 'Gallon', 'Millilitre']},
-                {name: 'Temperature', units: ['Celsius', 'Fahrenheit']},
+                {name: 'LENGTH', units: ['INCH', 'FEET', 'YARD', 'CENTIMETER']},
+                {name: 'WEIGHT', units: ['KILOGRAM', 'GRAM', 'TONNE']},
+                {name: 'VOLUME', units: ['LITRE', 'GALLON', 'MILLILITRE']},
+                {name: 'TEMPERATURE', units: ['CELSIUS', 'FAHRENHEIT']},
             ]
         });
     }
@@ -29,6 +34,27 @@ class QuantityConversion extends Component {
         this.setState({units: this.state.unitType.find(unitType => unitType.name === event.target.value).units});
     }
 
+    changeInputUnit(event) {
+        this.setState({inputUnit: event.target.value});
+    }
+
+    changeOutputUnit(event) {
+        this.setState({outputUnit: event.target.value});
+    }
+
+    setInput(event) {
+        this.setState({inputValue: event.target.value})
+    }
+
+    getConversion() {
+        axios.get("http://localhost:8081/unitConversion/addConversion/")
+            .then(response => {
+                this.setState({outputValue:response.data})
+            })
+            .catch(error => {
+                alert("Please enter proper value")
+            })
+    }
     render() {
         return (
             <div className='login-box'>
@@ -41,22 +67,24 @@ class QuantityConversion extends Component {
                 </select>
 
                 <br/><br/>
-                <input id="inputValue" type={"number"} placeholder='value'></input>
-                <input id="outputValue" type={"number"} placeholder='value'></input>
+                <input id="inputValue" type={"number"} placeholder='value' value={this.state.inputValue} onChange={this.setInput.bind(this)}></input>
+                <input id="outputValue" type={"number"} placeholder='value' value={this.state.outputValue} ></input>
                 <br/>
-                <select id="inputUnit">
-                    {this.state.units.map((e, key) => {
+                <select id="inputUnit" value={this.state.inputUnit} onChange={this.changeInputUnit.bind(this)}>
+                    {this.state.units.map((e, key) =>{
                         return <option key={key}>{e}</option>;
-                    })}
+                    })
+                    }
                 </select>
 
-                <select id="outputUnit">
+                <select id="outputUnit" value={this.state.outputUnit} onChange={this.changeOutputUnit.bind(this)}>
                     {this.state.units.map((e, key) => {
                         return <option key={key}>{e}</option>;
-                    })}
+                    })
+                    }
                 </select>
                 <br/><br/>
-                <button id="button" className='form'>Convert</button>
+                <button id="button" className='form' onClick={this.getConversion.bind(this)}>Convert</button>
             </div>
 
         )
